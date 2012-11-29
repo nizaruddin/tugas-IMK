eef8185bd713ee699b75b890b5373f25e353be11
nizaruddin <zard.puff@gmail.com>
2012-11-29 08:43:00 +0700

    update versi baru

---
+++ init.js
@@ -0,0 +1,53 @@
+﻿var init = {
+    'questions': [{
+        'question': 'FOL adalah kepanjangan dari',
+        'answers': ['A. First Order Logic', 'B. Fourth Order Logic', 'C. First Option Logic', 'D. Fourth Order Language'],
+        'correctAnswer': 1
+    }, {
+        'question': 'Alasan pendekatan FOL murni sulit dilakukan. kecuali..',
+        'answers': ['A. Biaya terlalu mahal', 'B. Interference terlalu repot', 'C. Tidak semua tes bisa dilakukan', 'D. banyak tes yang bisa dilakukan'],
+        'correctAnswer': 4
+    }, {
+        'question': 'Latar belakang munculnya Probabilistic Reasoning adalah..',
+        'answers': ['A. Terlalu banyak masalah yang diselesaikan', 'B. Dunia penuh dengan ketidakpastian', 'C. mempermudah interference', 'D. m\Mempermudah pencarian'],
+        'correctAnswer': 2
+    }, {
+        'question': 'Evaluation function yang digunakan untuk menilai semua tindakan adalah...',
+        'answers': ['A. Utility Theory', 'B. Probability Theory', 'C. Decision Theory', 'D. Urcentainty Theory'],
+        'correctAnswer': 1
+    }, {
+        'question': 'Biaya mahal dalam FOL termasuk dalam...',
+        'answers': ['A. Laziness', 'B. Ignorance', 'C. Practical Ignorance', 'D. Theorical Ignorance'],
+        'correctAnswer': 3
+    }, {
+        'question': 'Variabel yang ingin dihitung probabilitasnya disebut...',
+        'answers': ['A. Evidence Variable', 'B. Query Variable', 'C. Hidden Variable', 'D. Probabilistic Variable'],
+        'correctAnswer': 2
+    }, {
+        'question': 'Variabel yang ingin kita ketahui nilainya disebut...',
+        'answers': ['A. Evidence Variable', 'B. Query Variable', 'C. Hidden Variable', 'D. Probabilistic Variable'],
+        'correctAnswer': 1
+    }, {
+        'question': 'Bayes Rule akan menjadi efektif jika dikombinasikan dengan...',
+        'answers': ['A. Probabilistic Independence', 'B. Probabilistic Reasoning', 'C. Conditional Independence', 'D. Conditional Reasoning'],
+        'correctAnswer': 3
+    }, {
+        'question': 'Bayes Rule digunakan untuk...',
+        'answers': ['A. Mempermudah pencarian solusi', 'B. mempermudah pencarian akibat', 'C. Mengetahui sebab jika diketahui akibat', 'D. Mengetahui akibat jika diketahui sebab'],
+        'correctAnswer': 4
+    }, {
+        'question': 'Variabel yang belum diketahui disebut',
+        'answers': ['A. Evidence Variable', 'B. Query Variable', 'C. Hidden Variable', 'D. Probabilistic Variable'],
+        'correctAnswer': 3
+    }, ],
+    'resultComments': {
+        perfect: 'Wow, Kamu Jenius Sekali!',
+        excellent: 'Mengagumkan! Kamu pintar!',
+        good: 'Bagus, pertahakan prestasimu!',
+        average: 'Pokoknya lulus nilai minimal kan?',
+        bad: 'Sayang sekali',
+        poor: 'Belajar lebih giat!',
+        worst: 'Kamu bercanda? Kerjakan dengan serius!'
+    }
+
+};
